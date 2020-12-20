ed9609317ba57a985364a9da74759ed5a5e6e8a5
mio <leftyfair@gmail.com>
2020-12-20 22:50:17 +0900

    getMovies() 함수와 지연시키는 async와 await

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   state = {
@@ -6,11 +7,13 @@ class App extends React.Component {
     movies: [],
   };
 
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+  }
+
   componentDidMount() {
     // 영화 데이터 로딩
-    setTimeout(() => {
-      this.setState({ isLoading: false});
-    }, 6000);
+    this.getMovies();
   }
   
   render() {
